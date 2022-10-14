78dc00f8e5b77fa988a7cbac7b6bd248fec57564
marvelxcodes <ramakrishnan0046@gmail.com>
2022-10-14 21:55:58 +0530

    Fibonacci Series

--- Fibonacci.js
+++ Fibonacci.js
@@ -1,13 +1,17 @@
+// * Fibonacci series ( 0 1 1 2 3 5 8 13 21 34... )
 
-// Basic
-function Fibonacci(n) {
-    let prev = 0; let curr = 1; let i = 1
+// Noobie
+function Fibonacci_Noob(n) {
+    const series = []
+    let prev = 0; let curr = 1; let i = 0
     
-    while (i<=n) {
-        console.log(prev)
+    while (i<n) {
+        series[i] = prev
         let temp = curr
         curr = prev+curr
         prev = temp
         i++
     }
+    return series
 }
+
